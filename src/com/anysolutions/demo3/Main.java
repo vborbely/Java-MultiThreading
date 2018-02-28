ed4f6e5adbeef920d5690d7fe3325b325b063d9b
vborbely <borbelyv@gmail.com>
2018-02-28 22:41:34 +0100

    Demo3 defining Runnable via anonymus class.

--- src/com/anysolutions/demo3/Main.java
+++ src/com/anysolutions/demo3/Main.java
@@ -1,4 +1,26 @@
-package com.com.anysolutions.demo3;
+package com.anysolutions.demo3;
 
 public class Main {
+
+    public static void main(String args[]) {
+        // Runnable with Anonymus class
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Thread #" + i);
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t1.start();
+    }
+
 }
