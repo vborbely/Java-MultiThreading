08cb9934c26f3b196e918168a260caf462febe82
vborbely <borbelyv@gmail.com>
2018-03-05 22:34:03 +0100

    Thread pools with ExecutorService.

--- src/com/anysolutions/Threading5/Main.java
+++ src/com/anysolutions/Threading5/Main.java
@@ -1,7 +1,6 @@
 package com.anysolutions.Threading5;
 
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
