39e85e4fed39a0c60f204168adaa99d46bfd70f8
vborbely <borbelyv@gmail.com>
2018-03-05 22:33:24 +0100

    Thread pools with ExecutorService.

---
+++ src/com/anysolutions/Threading5/Main.java
@@ -0,0 +1,50 @@
+package com.anysolutions.Threading5;
+
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Processor implements Runnable {
+    private int id;
+
+    Processor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Staring: " + id);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed: " + id);
+    }
+}
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++) {
+            executor.submit(new Processor(i));
+        }
+
+        System.out.println("All tasks submitted.");
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All tasks completed.");
+    }
+}
